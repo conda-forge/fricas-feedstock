71cc7520e0faea3fb759efbaed685e1058dd4e43
cxzhong <chenxin.zhong@outlook.com>
2026-03-07 18:52:25 +0800

    fix the error, and ppc64le is not compatible

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# SBCL-generated executables reference libzstd via @rpath; ensure it's findable
+export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib${DYLD_FALLBACK_LIBRARY_PATH:+:$DYLD_FALLBACK_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+
 ./configure --prefix="$PREFIX" --with-lisp=sbcl --enable-lisp-core
 make
 make install
